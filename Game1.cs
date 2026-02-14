aea02833d9e6ce993e8ef6f045c7cfea8f0892b5
paolo9688 <paolo.pz1@gmail.com>
2026-02-14 16:21:47 +0100

    Introdotto il player

--- Game1.cs
+++ Game1.cs
@@ -9,8 +9,8 @@ namespace Test_Monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        // Riferimento alla nostra classe mappa
         private MapManager _mapManager;
+        private Player _player;
 
         public Game1()
         {
@@ -25,8 +25,8 @@ namespace Test_Monogame
 
         protected override void Initialize()
         {
-            // Inizializziamo l'oggetto mappa
             _mapManager = new MapManager();
+            _player = new Player(new Vector2(100, 100));
 
             base.Initialize();
         }
@@ -35,8 +35,8 @@ namespace Test_Monogame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Carichiamo gli asset della mappa
             _mapManager.LoadContent(Content);
+            _player.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,7 +45,7 @@ namespace Test_Monogame
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Qui in futuro chiameremo _player.Update(gameTime);
+            _player.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -61,7 +61,7 @@ namespace Test_Monogame
             // Disegniamo la mappa
             _mapManager.Draw(_spriteBatch);
 
-            // Qui in futuro chiameremo _player.Draw(_spriteBatch);
+            _player.Draw(_spriteBatch);
 
             _spriteBatch.End();
 
---
+++ Player.cs
@@ -0,0 +1,55 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Player
+{
+    private Texture2D _spriteSheet;
+    private Vector2 _position;
+    private float _speed = 150f; // Pixel al secondo
+
+    // Definiamo la dimensione della singola sprite
+    private int _spriteWidth = 32;
+    private int _spriteHeight = 32;
+
+    public Player(Vector2 startPosition)
+    {
+        _position = startPosition;
+    }
+
+    public void LoadContent(ContentManager content)
+    {
+        // Assicurati che il nome "PlayerSheet" corrisponda a quello nel MGCB Editor
+        _spriteSheet = content.Load<Texture2D>("Player");
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        KeyboardState state = Keyboard.GetState();
+        Vector2 direction = Vector2.Zero;
+
+        // Input semplice per movimento scattante
+        if (state.IsKeyDown(Keys.W) || state.IsKeyDown(Keys.Up)) direction.Y -= 1;
+        if (state.IsKeyDown(Keys.S) || state.IsKeyDown(Keys.Down)) direction.Y += 1;
+        if (state.IsKeyDown(Keys.A) || state.IsKeyDown(Keys.Left)) direction.X -= 1;
+        if (state.IsKeyDown(Keys.D) || state.IsKeyDown(Keys.Right)) direction.X += 1;
+
+        // Normalizziamo la direzione per evitare che in diagonale vada più veloce
+        if (direction != Vector2.Zero)
+        {
+            direction.Normalize();
+            _position += direction * _speed * deltaTime;
+        }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        // Sorgente: Riga 1, Colonna 1 (Coordinate 0,0)
+        Rectangle sourceRect = new Rectangle(0, 0, _spriteWidth, _spriteHeight);
+
+        // Disegniamo il player
+        spriteBatch.Draw(_spriteSheet, _position, sourceRect, Color.White);
+    }
+}
